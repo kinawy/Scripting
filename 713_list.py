0d07945c2290b2ca8835de57b454207a70702743
kinawy <sameh.yasser.kinawy@gmail.com>
2020-09-16 19:54:11 -0400

    Fixes line break

--- 713_list.py
+++ 713_list.py
@@ -31,7 +31,8 @@ squad_713 = [
 
 ga_file = open('ga.txt', 'w')
 for student in squad_713:
-    ga_file.writelines(student)
+    ga_file.write("\n")
+    ga_file.write(student)
     
 ga_file.close()
 
